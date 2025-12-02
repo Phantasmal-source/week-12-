8f50bf75e5f89fbf724b6c8b1e9102757f830a43
Phantasmal-source <joahuawoodward@gmail.com>
2025-12-02 19:25:27 +0000

    temperatureeeeee

--- week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
+++ week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
@@ -5,9 +5,18 @@
 # Write a program that:
 
 # Asks the user for today’s temperature.
-
+temperature = int(input("What's the fahrenheit temperature bro?: "))
 # Prints whether it’s cold, warm, or hot using comparison operators.
-
+if temperature < 75 and temperature > -10:
+    print("its cool!")
+else:
+    if temperature >= 75 and temperature <= 90:
+        print("it's warm")
+    else:
+        if temperature > 90 and temperature < 110:
+            print("Too hot!")
+        else:
+            print("Extreme temperature warning!")
 # If the temperature is out of range (below -10 or above 110), display “Extreme temperature warning!”
 
 # Starter Code:
